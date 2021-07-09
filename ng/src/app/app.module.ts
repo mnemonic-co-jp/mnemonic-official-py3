5fcf361f21a711ed5ab2d668fb242f3c34673f68
Somin Kobayashi <somin@mnemonic.co.jp>
2021-07-09 20:17:36 +0900

    bootstrap 導入＋API コールのテストなど

--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { TestComponent } from './test/test.component';
 import { CoreInterceptor } from './shared/interceptors/core.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     TestComponent
   ],
   imports: [
