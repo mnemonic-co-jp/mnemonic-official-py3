645843a5ef6b2d63147b1ffd596d2ac8bcee5bb0
Somin Kobayashi <somin@mnemonic.co.jp>
2022-02-08 19:25:51 +0900

    TailwindCSS を導入

---
+++ ng/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'mnemonic-orange': '#ff9326'
+      },
+      fontFamily: {
+        sans: ['system-ui', '-apple-system', 'IBM Plex Sans JP', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
