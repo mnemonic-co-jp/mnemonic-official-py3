3e4dc5c7da20d9facfde00bc87ba56141a36da89
Somin Kobayashi <somin@mnemonic.co.jp>
2026-03-23 03:38:37 +0900

    「弊社について」ページを移植

--- ng/src/app/base/base.component.ts
+++ ng/src/app/base/base.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { Title } from "@angular/platform-browser";
 import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 import { format } from 'date-fns';
 import { ToastService } from '../shared/services/toast.service';
+
+interface ComponentRef {
+  name?: string;
+  title?: string;
+}
+
+const BASE_TITLE = 'ニモニク - 株式会社ニーモニック';
 
 @Component({
   standalone: true,
@@ -20,9 +28,19 @@ export class BaseComponent {
   isHome: boolean = true;
   currentYear: string = format(new Date(), 'yyyy');
 
-  constructor(public toastService: ToastService) {};
+  constructor(
+    private title: Title,
+    public toastService: ToastService
+  ) {
+    this.title.setTitle(BASE_TITLE);
+  };
 
-  onActivate(componentRef: any) {
-    this.isHome = componentRef?.name === 'home';
+  onActivate(componentRef: ComponentRef) {
+    // NOTE: 変更タイミングをずらさないと NG0100 のエラーになる
+    setTimeout(() => this.isHome = componentRef?.name === 'home');
+    const title = componentRef?.title;
+    if (title) {
+      this.title.setTitle(title ? `${title} | ${BASE_TITLE}` : BASE_TITLE);
+    }
   }
 }
