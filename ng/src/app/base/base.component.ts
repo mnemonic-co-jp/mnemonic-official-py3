93315e8999e3da56a8b2b5497c57212bd3d7e3f7
Somin Kobayashi <somin@mnemonic.co.jp>
2026-03-30 23:45:07 +0900

    Google Adsense 広告はディレクティブではなく直接埋め込む

--- ng/src/app/base/base.component.ts
+++ ng/src/app/base/base.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, AfterViewInit, Inject } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { Title } from "@angular/platform-browser";
 import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 import { format } from 'date-fns';
-import { GoogleAdsenseDirective } from '../shared/directives/google-adsense.directive';
 import { ToastService } from '../shared/services/toast.service';
 
 interface ComponentRef {
@@ -20,22 +19,37 @@ const BASE_TITLE = 'ニモニク - 株式会社ニーモニック';
     CommonModule,
     RouterModule,
     RouterOutlet,
-    NgbToastModule,
-    GoogleAdsenseDirective
+    NgbToastModule
   ],
   templateUrl: './base.component.html',
   styleUrls: ['./base.component.scss']
 })
-export class BaseComponent {
+export class BaseComponent implements AfterViewInit {
   isHome: boolean = true;
   currentYear: string = format(new Date(), 'yyyy');
 
   constructor(
+    @Inject(DOCUMENT) private document: Document,
     private titleService: Title,
     public toastService: ToastService
   ) {
     this.titleService.setTitle(BASE_TITLE);
   };
+
+  ngAfterViewInit(): void {
+    const hostElement = this.document.getElementById('google-adsense');
+    if (!hostElement) {
+      return;
+    }
+    const script = this.document.createElement('script');
+    script.type = 'text/javascript';
+    script.id = `mini-skyscraper-${new Date().getTime()}`;
+    script.text = '(adsbygoogle = window.adsbygoogle || []).push({});';
+    const parent = hostElement.parentNode;
+    if (parent) {
+      parent.replaceChild(script, hostElement);
+    }
+  }
 
   onActivate(componentRef: ComponentRef) {
     // NOTE: 変更タイミングをずらさないと NG0100 のエラーになる
