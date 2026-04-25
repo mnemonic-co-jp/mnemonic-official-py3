d5106bc634e69f16d162acb30ff9050bb8bbdf18
Somin Kobayashi <somin@mnemonic.co.jp>
2026-04-26 05:07:30 +0900

    admin の API が 401 を返した際の処理を追加

--- src/app/shared/interceptors/google-auth.interceptor.ts
+++ src/app/shared/interceptors/google-auth.interceptor.ts
@@ -1,10 +1,13 @@
 import { inject } from '@angular/core';
-import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { OAuthStorage } from 'angular-oauth2-oidc';
+import { GoogleAuthService } from '../../shared/services/google-auth.service';
 
 export const googleAuthInterceptor: HttpInterceptorFn = (request: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
   const oAuthStorage = inject(OAuthStorage);
+  const authService = inject(GoogleAuthService);
   if (request.url.startsWith('/admin/api/') && !request.headers.has('Authorization')) {
     const token: string | null = oAuthStorage.getItem('id_token');
     if (token) {
@@ -15,5 +18,14 @@ export const googleAuthInterceptor: HttpInterceptorFn = (request: HttpRequest<un
       });
     }
   }
-  return next(request);
-}
+  return next(request).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        authService.logout();
+        window.location.reload();
+        return next(request);
+      }
+      return throwError(() => error);
+    })
+  );
+};
